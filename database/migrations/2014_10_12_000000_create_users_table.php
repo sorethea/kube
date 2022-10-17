ae4048e2dc93d01542646e7a8e6b7f6947d80527
Sorethea OK <sorethea@gmail.com>
2022-10-17 14:26:58 +0700

    Disabled kubeadmin package

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('device_token')->nullable();
+            $table->timestamp('verified_at')->nullable();
+            $table->boolean('active')->default(true);
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
