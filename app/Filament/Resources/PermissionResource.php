3eef48dfe56d4c85fbc0c368bea1e0ea426efaca
Sorethea OK <sorethea@gmail.com>
2022-10-18 17:02:24 +0700

    install spatie media library

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -19,7 +19,7 @@ class PermissionResource extends Resource
 {
     protected static ?string $model = Permission::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-key';
 
     protected static function getNavigationGroup(): ?string
     {
