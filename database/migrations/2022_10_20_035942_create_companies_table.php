ea6089468e9b05d02cd191b5134f03197a9a1fdf
Sorethea OK <sorethea@gmail.com>
2022-10-20 11:13:31 +0700

    install spatie media library

---
+++ database/migrations/2022_10_20_035942_create_companies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('abbr',10)->unique();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('website')->nullable();
+            $table->string('address')->nullable();
+            $table->string('location')->nullable();
+            $table->string('default_currency')->nullable();
+            $table->text('description')->nullable();
+            $table->text('detail')->nullable();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
