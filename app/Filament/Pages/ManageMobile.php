c4db37ef1feb084c8492055f285b0787426407aa
Sorethea OK <sorethea@gmail.com>
2022-10-27 09:42:08 +0700

    CompanyResource.php

--- app/Filament/Pages/ManageMobile.php
+++ app/Filament/Pages/ManageMobile.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Pages;
 
 use App\Settings\MobileSettings;
+use Filament\Forms\Components\KeyValue;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\SettingsPage;
@@ -21,17 +22,18 @@ class ManageMobile extends SettingsPage
     protected function getFormSchema(): array
     {
         return [
-            TextInput::make("general.app_name"),
-            MarkdownEditor::make("general.description"),
-            TextInput::make("general.main_color"),
-            TextInput::make("general.second_color"),
-            TextInput::make("general.accent_color"),
-            TextInput::make("general.scaffold_color"),
-            TextInput::make("general.main_dark_color"),
-            TextInput::make("general.second_dark_color"),
-            TextInput::make("general.accent_dark_color"),
-            TextInput::make("general.scaffold_dark_color"),
-            TextInput::make("general.mobile_language"),
+//            TextInput::make("general.app_name"),
+//            MarkdownEditor::make("general.description"),
+//            TextInput::make("general.main_color"),
+//            TextInput::make("general.second_color"),
+//            TextInput::make("general.accent_color"),
+//            TextInput::make("general.scaffold_color"),
+//            TextInput::make("general.main_dark_color"),
+//            TextInput::make("general.second_dark_color"),
+//            TextInput::make("general.accent_dark_color"),
+//            TextInput::make("general.scaffold_dark_color"),
+//            TextInput::make("general.mobile_language"),៊
+            KeyValue::make("generale")
         ];
     }
 }
