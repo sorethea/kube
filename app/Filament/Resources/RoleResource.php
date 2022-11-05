8e9a6cbd86efd19272fbaac3f763aa71ececb583
Sorethea OK <sorethea@gmail.com>
2022-11-05 11:17:53 +0700

    CompanyResource.php

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -66,7 +66,7 @@ class RoleResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+           RelationManagers\PermissionsRelationManager::class,
         ];
     }
 
