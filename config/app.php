4d115c3994a69f01f286503891b3ccc86d3e8123
Sorethea OK <sorethea@gmail.com>
2022-11-14 08:40:00 +0700

    Ignore /packages

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         //Sorethea\Assign\AssignServiceProvider::class,
+        Sorethea\LaravelState\StateServiceProvider::class,
     ],
 
     /*
