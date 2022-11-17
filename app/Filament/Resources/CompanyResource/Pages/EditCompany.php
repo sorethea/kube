31f54c1bcded1dcc7c18eab025f592790fff17f6
Sorethea OK <sorethea@gmail.com>
2022-11-17 16:21:02 +0700

    Sorethea/document-state

--- app/Filament/Resources/CompanyResource/Pages/EditCompany.php
+++ app/Filament/Resources/CompanyResource/Pages/EditCompany.php
@@ -14,8 +14,14 @@ class EditCompany extends EditRecord
     protected function getActions(): array
     {
         return [
-            Actions\Action::make("submit")->action(fn($record)=>$record->setState(1))->color("success"),
-            Actions\Action::make("cancel")->action(fn($record)=>$record->setState(2))->color("warning"),
+            Actions\Action::make("submit")
+                ->requiresConfirmation()
+                ->action(fn($record)=>$record->setState(1))
+                ->color("success"),
+            Actions\Action::make("cancel")
+                ->requiresConfirmation()
+                ->action(fn($record)=>$record->setState(2))
+                ->color("warning"),
             Actions\DeleteAction::make(),
         ];
     }
