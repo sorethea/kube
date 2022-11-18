30af18a03917943b125889469bd4f7517b590410
Sorethea OK <sorethea@gmail.com>
2022-11-18 09:10:27 +0700

    Sorethea/document-state

--- app/Filament/Resources/CompanyResource/Pages/EditCompany.php
+++ app/Filament/Resources/CompanyResource/Pages/EditCompany.php
@@ -19,13 +19,18 @@ class EditCompany extends EditRecord
             Actions\Action::make("submit")
                 ->requiresConfirmation()
                 ->action(fn()=>$this->record->setState(1))
-                ->visible(fn()=>$this->record->state==0 && auth()->user()->can("companies.manager"))
+                ->visible(fn()=>$this->record->state==0 && auth()->user()->can("companies.submit"))
                 ->color("success"),
             Actions\Action::make("cancel")
                 ->requiresConfirmation()
                 ->action(fn()=>$this->record->setState(2))
-                ->visible(fn()=>$this->record->state!=2 && auth()->user()->can("companies.manager"))
+                ->visible(fn()=>$this->record->state!=2 && auth()->user()->can("companies.cancel"))
                 ->color("warning"),
+            Actions\Action::make("restore")
+                ->requiresConfirmation()
+                ->action(fn()=>$this->record->setState(0))
+                ->visible(fn()=>$this->record->state==2 && auth()->user()->can("companies.restore"))
+                ->color("primary"),
             Actions\DeleteAction::make(),
         ];
     }
