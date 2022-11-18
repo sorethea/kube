c5e923400458bfd508ea8a1200205be7d769a6b3
Sorethea OK <sorethea@gmail.com>
2022-11-18 08:46:22 +0700

    Sorethea/document-state

--- app/Filament/Resources/CompanyResource.php
+++ app/Filament/Resources/CompanyResource.php
@@ -68,6 +68,10 @@ class CompanyResource extends Resource
                     0 => config("document-state.status")[0],
                     1 => config("document-state.status")[1],
                     2 => config("document-state.status")[2],
+                ])->color([
+                    0=>"primary",
+                    1=>"success",
+                    2=>"danger",
                 ]),
             ])
             ->filters([
