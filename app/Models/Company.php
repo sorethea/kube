88a73cceb2d295cce188fefe4c08be248cb3d2cc
Sorethea OK <sorethea@gmail.com>
2022-11-18 14:30:25 +0700

    Sorethea/document-state

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\ActiveScope;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Sorethea\DocumentState\Traits\DocumentStateTrait;
