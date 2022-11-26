44274ed53ddc828e1d8fe10451eeb9c47b34b4b7
Sorethea OK <sorethea@gmail.com>
2022-11-26 09:31:04 +0700

    Sorethea/document-state

--- app/Filament/Resources/CompanyResource/Pages/EditCompany.php
+++ app/Filament/Resources/CompanyResource/Pages/EditCompany.php
@@ -24,7 +24,7 @@ class EditCompany extends EditRecord
                 ->form([
                     TextInput::make("title")->required(),
                     MarkdownEditor::make("comment")->required(),
-                    MultiSelect::make("assigner")->options(app(config("todo.causer"))->all()->pluck("name","id"))
+                    MultiSelect::make("assigner")->options(config("todo.causer")::all()->pluck("name","id"))
                 ])
                 //->requiresConfirmation()
                 ->action(fn()=>$this->record->setState(1))
