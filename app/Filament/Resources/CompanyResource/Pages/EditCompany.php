96a55e366db5fdb57033d89f216f0239f353a689
Sorethea OK <sorethea@gmail.com>
2022-11-26 09:11:12 +0700

    Sorethea/document-state

--- app/Filament/Resources/CompanyResource/Pages/EditCompany.php
+++ app/Filament/Resources/CompanyResource/Pages/EditCompany.php
@@ -5,7 +5,10 @@ namespace App\Filament\Resources\CompanyResource\Pages;
 use App\Filament\Resources\CompanyResource;
 use App\Models\Company;
 use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\BelongsToManyMultiSelect;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\MultiSelect;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
@@ -19,10 +22,12 @@ class EditCompany extends EditRecord
         return [
             Actions\Action::make("assign")
                 ->form([
+                    TextInput::make("title")->required(),
                     MarkdownEditor::make("comment")->required(),
+                    BelongsToManyMultiSelect::make("causer")->label("Reviewer")->relationship("causer","name")->required(),
                 ])
                 //->requiresConfirmation()
-                ->action(fn()=>$this->record->setState(1))
+                //->action(fn()=>$this->record->setState(1))
                 ->visible(fn()=>$this->record->state==0 && auth()->user()->can("companies.submit"))
                 ->color("success"),
             Actions\Action::make("cancel")
